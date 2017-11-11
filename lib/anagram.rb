c1d9f9d22d34f6db0a6b53d7d5a708c6037923fa
Morgan Fudge <mfudge92@gmail.com>
2017-11-11 19:43:05 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,8 +8,12 @@ class Anagram
   end
 
   def match(arr)
+    match = Array.new
     arr.each_with_index do |k, v|
       search = v.split.sort
+      if search == @word
+        match <<  v
+      end
     end
   end
 
