b18394e4bfc9618dd33a2e9617a5651260a30de4
Morgan Fudge <mfudge92@gmail.com>
2017-11-11 19:41:24 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,12 +4,12 @@ class Anagram
   attr_accessor :word
 
   def initialize(word)
-    @word = word
+    @word = word.split
   end
 
   def match(arr)
     arr.each_with_index do |k, v|
-
+      
     end
   end
 
